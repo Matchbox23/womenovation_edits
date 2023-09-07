14e84d7ba98cd5e9d8d91ffbc0c22434c0243539
Yousef Korayem <39414099+Matchbox23@users.noreply.github.com>
2023-09-07 15:16:44 +0300

    Toggleable music, changed womenovation name font, removed background colors from navbars, fixed mobile edge boundaries fault, redid footer logo

--- js/main.js
+++ js/main.js
@@ -183,3 +183,16 @@ allLinks.forEach(function (link) {
     }
   });
 });
+
+function toggleMute() {
+  var button=document.getElementById("MuteButton")
+  var video=document.getElementById("bgVideo")
+  
+  if(video.muted){
+    video.muted = false;
+  } else {
+    video.muted = true;
+  }
+  
+  button.classList.toggle('muted');
+}
